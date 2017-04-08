4bd0ce49ef03f424dd6cde6b98c237bcae32994a
baileyal <baileyal@oregonstate.edu>
2017-04-08 15:02:37 -0700

    pushing new kinect_test

--- visual studio files/kinect_test/WindowsFormsApplication3/Form1.Designer.cs
+++ visual studio files/kinect_test/WindowsFormsApplication3/Form1.Designer.cs
@@ -29,44 +29,31 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.optionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btn_kinect_stream = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(40, 36);
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(16, 90);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(211, 144);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.Size = new System.Drawing.Size(291, 271);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(3, 205);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(188, 21);
-            this.comboBox1.TabIndex = 1;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(189, 203);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Start Cam";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // menuStrip1
             // 
@@ -75,7 +62,7 @@
             this.optionToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(692, 24);
             this.menuStrip1.TabIndex = 3;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -91,13 +78,13 @@
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
             this.saveToolStripMenuItem.Text = "Save";
             // 
             // loadToolStripMenuItem
             // 
             this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
-            this.loadToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
             this.loadToolStripMenuItem.Text = "load";
             // 
             // optionToolStripMenuItem
@@ -106,13 +93,56 @@
             this.optionToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
             this.optionToolStripMenuItem.Text = "Option";
             // 
+            // btn_kinect_stream
+            // 
+            this.btn_kinect_stream.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_kinect_stream.Location = new System.Drawing.Point(541, 419);
+            this.btn_kinect_stream.Name = "btn_kinect_stream";
+            this.btn_kinect_stream.Size = new System.Drawing.Size(118, 23);
+            this.btn_kinect_stream.TabIndex = 4;
+            this.btn_kinect_stream.Text = "Stream";
+            this.btn_kinect_stream.UseVisualStyleBackColor = true;
+            this.btn_kinect_stream.Click += new System.EventHandler(this.kinect_stream_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Status:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(72, 28);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(10, 13);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "-";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.Location = new System.Drawing.Point(373, 90);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(297, 271);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(692, 454);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_kinect_stream);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -123,6 +153,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -131,13 +162,15 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem optionToolStripMenuItem;
+        private System.Windows.Forms.Button btn_kinect_stream;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- visual studio files/kinect_test/WindowsFormsApplication3/Form1.cs
+++ visual studio files/kinect_test/WindowsFormsApplication3/Form1.cs
@@ -10,12 +10,17 @@ using System.Windows.Forms;
 using AForge.Video;
 using AForge.Video.DirectShow;
 
+using Microsoft.Kinect;
+using Coding4Fun.Kinect.WinForm;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.IO;
+
 namespace WindowsFormsApplication3
 {
     public partial class Form1 : Form
     {
-        private FilterInfoCollection VideoCaptureDevices;//stores all video devices available
-        private VideoCaptureDevice FinalVideoSource;//store device to be used
+        private KinectSensor kSensor;
 
         public Form1()
         {
@@ -24,21 +29,10 @@ namespace WindowsFormsApplication3
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            VideoCaptureDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice); //store capture devices in this array
-            foreach(FilterInfo VideoCaptureDevice in VideoCaptureDevices)
-            {
-                comboBox1.Items.Add(VideoCaptureDevice.Name);//add name to combobox1
-            }
-
-            comboBox1.SelectedIndex = 0;
+            
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            FinalVideoSource = new VideoCaptureDevice(VideoCaptureDevices[comboBox1.SelectedIndex].MonikerString); //set selected video as source
-            FinalVideoSource.NewFrame += new NewFrameEventHandler(FinalVideoSource_NewFrame);
-            FinalVideoSource.Start();
-        }
+       
 
         private void FinalVideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
         {
@@ -50,10 +44,331 @@ namespace WindowsFormsApplication3
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
             //stop webcam when form closes
-            if(FinalVideoSource.IsRunning)
+            if (kSensor != null && kSensor.IsRunning)
             {
-                FinalVideoSource.Stop();
+                kSensor.Stop();
+            }
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kinect_stream_Click(object sender, EventArgs e)
+        {
+            if (btn_kinect_stream.Text == "Stream")
+            {
+                if (KinectSensor.KinectSensors.Count > 0)
+                {
+                    this.btn_kinect_stream.Text = "Stop";
+                    kSensor = KinectSensor.KinectSensors[0];
+                    KinectSensor.KinectSensors.StatusChanged += KinectSensors_StatusChanged;
+                }
+
+                kSensor.Start();
+                kSensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                kSensor.ColorFrameReady += KSensor_ColorFrameReady;
+
+                kSensor.DepthStream.Enable();
+                kSensor.DepthStream.Range = DepthRange.Default;
+                kSensor.DepthFrameReady += KSensor_DepthFrameReady;
+
+                //kSensor.AllFramesReady += KSensor_AllFramesReady;
+                //kSensor.SkeletonStream.Enable();
+                //kSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+
+            }
+            else
+            {
+                if(kSensor != null && kSensor.IsRunning)
+                {
+                    kSensor.Stop();
+                    this.btn_kinect_stream.Text = "Stream";
+                    this.pictureBox1.Image = null;
+                }
+            }
+
+            
+        }
+
+        private void KSensor_DepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
+        {
+            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+            {
+                if(depthFrame != null)
+                {
+                    //pictureBox1.Image = CreateColorBitmapFromDepth(depthFrame);
+
+                    byte[] pixels = GenerateColoredBytes(depthFrame);
+
+                    //number of bytes per row width * 4 (B,G,R, Empty)
+                    int stride = depthFrame.Width * 4;
+
+                    //create image
+                    pictureBox1.Image = BitmapFromSource( BitmapSource.Create(depthFrame.Width, depthFrame.Height, 96, 96, PixelFormats.Bgr32, null, pixels, stride) );
+                }
             }
         }
+
+        
+
+        private void KSensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            using (var frame = e.OpenColorImageFrame())
+            {
+                if (frame != null)
+                {
+                    //pictureBox2.Image = CreateBitmapFromSensor(frame);
+                }
+            }
+
+            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+            {
+                if(depthFrame != null)
+                {
+                    //byte[] pixels = GenerateColoredBytes(depthFrame);
+
+                    //number of bytes per row width * 4 (B,G,R, Empty)
+                    int stride = depthFrame.Width * 4;
+
+                    //create image
+                    //pictureBox1.Image = BitmapFromSource( BitmapSource.Create(depthFrame.Width, depthFrame.Height, 96, 96, PixelFormats.Bgr32, null, pixels, stride) );
+                    //pictureBox1.Image = CreateColorBitmapFromDepth(depthFrame);
+
+
+                }
+            }
+
+            /*
+            using (var frame = e.OpenSkeletonFrame())
+            {
+                if(frame == null)
+                {
+                    return;
+                }
+
+                var skeletons = new Skeleton[frame.SkeletonArrayLength];
+                frame.CopySkeletonDataTo(skeletons);
+
+                var trackedSkeleton = skeletons.FirstOrDefault(s => s.TrackingState == SkeletonTrackingState.Tracked);
+                if(trackedSkeleton == null)
+                {
+                    return;
+                }
+
+                var position = trackedSkeleton.Joints[JointType.HandRight].Position;
+
+                
+
+
+            }
+            //*/
+        }
+
+        private byte[] GenerateColoredBytes(DepthImageFrame depthFrame)
+        {
+
+            //get min and max reliable depth
+            int minDepth = depthFrame.MinDepth;
+            int maxDepth = depthFrame.MaxDepth;
+
+            //get the raw data from kinect with the depth for evey pixel
+            short[] rawDepthData = new short[depthFrame.PixelDataLength];
+            depthFrame.CopyPixelDataTo(rawDepthData);
+
+            //using the depthFrame to creat the image to display on-screen
+            //depthFrame contains color information for all pixel in image
+            //Height x Width x 4 (Red, Green, Blue, empty byte)
+            Byte[] pixels = new byte[depthFrame.Height * depthFrame.Width * 4];
+
+            //Bgr32  - Blue, Green, Red. empty byte
+            //Bgra32 - Blue, Green, Red, transparency
+            //you must set transparency for Bgra as .NET defaults a byte to 0 = fully transparent
+
+            //hardcoded location to Blue, Green, Red (BGR) index postitions
+            const int BlueIndex = 0;
+            const int GreenIndex = 1;
+            const int RedIndex = 2;
+
+            //loop through all distances
+            //pick a RGB color based on distance
+            for (int depthIndex = 0, colorIndex = 0;
+                depthIndex < rawDepthData.Length && colorIndex < pixels.Length;
+                depthIndex++, colorIndex += 4)
+            {
+
+                
+                //get the depth value
+                int depth = rawDepthData[depthIndex] >> DepthImageFrame.PlayerIndexBitmaskWidth;
+
+                /*
+                //.9M or 2.95
+                if(depth <= 900)
+                {
+                    //we are very close
+                    pixels[colorIndex + BlueIndex] = 255;
+                    pixels[colorIndex + GreenIndex] = 0;
+                    pixels[colorIndex + RedIndex] = 0;
+                }
+                //.9M - 2M or 2.95' - 6.56'
+                else if(depth > 900 && depth < 2000)
+                {
+                    //we are a bit further away
+                    pixels[colorIndex + BlueIndex] = 0;
+                    pixels[colorIndex + GreenIndex] = 255;
+                    pixels[colorIndex + RedIndex] = 0;
+                }
+                //2M+ or 6.56'+
+                else if(depth > 200)
+                {
+                    //we are the farthest
+                    pixels[colorIndex + BlueIndex] = 255;
+                    pixels[colorIndex + GreenIndex] = 0;
+                    pixels[colorIndex + RedIndex] = 0;
+                }
+                //*/
+
+                //equal coloring for the monochromatic histogram
+                byte intensity = CalculateIntesityFromDepth(depth, minDepth, maxDepth);
+                pixels[colorIndex + BlueIndex] = intensity;
+                pixels[colorIndex + GreenIndex] = intensity;
+                pixels[colorIndex + RedIndex] = intensity;
+            }
+
+            return pixels;
+        }
+
+        private byte CalculateIntesityFromDepth(int distance, int MinDepthDistance, int MaxDepthDistance)
+        {
+
+            //formula for calculating monochrome intensity for histogram
+            return (byte)(255 - (255 * Math.Max(distance - MinDepthDistance, 0)
+                / (MaxDepthDistance)));
+        }
+
+        private void KSensor_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (var frame = e.OpenColorImageFrame())
+            {
+                if (frame != null)
+                {
+                    //create bitmap from sensor
+                    pictureBox2.Image = CreateBitmapFromSensor(frame);
+                }
+            }
+        }
+
+        private void KinectSensors_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            this.lblStatus.Text = kSensor.Status.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private Bitmap CreateBitmapFromSensor(ColorImageFrame frame)
+        {
+            //WriteableBitmap map;
+            var pixelData = new byte[frame.PixelDataLength];
+            frame.CopyPixelDataTo(pixelData);
+
+           // return pixelData.ToBitmap(frame.Width, frame.Height);
+
+            //*
+            var stride = frame.Width * frame.BytesPerPixel;
+
+            var bmpFrame = new Bitmap(frame.Width, frame.Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+            var bmpData = bmpFrame.LockBits(new Rectangle(0, 0, frame.Width, frame.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, bmpFrame.PixelFormat);
+
+            System.Runtime.InteropServices.Marshal.Copy(pixelData, 0, bmpData.Scan0, frame.PixelDataLength);
+
+            bmpFrame.UnlockBits(bmpData);
+
+            return bmpFrame;
+            //*/
+        }
+
+        private Bitmap CreateColorBitmapFromDepth(DepthImageFrame frame)
+        {
+            //WriteableBitmap colorBitmap;
+            DepthImagePixel[] depthPixels;
+            byte[] colorPixels;
+
+            depthPixels = new DepthImagePixel[kSensor.DepthStream.FramePixelDataLength];
+            colorPixels = new byte[kSensor.DepthStream.FramePixelDataLength * sizeof(int)];
+
+            frame.CopyDepthImagePixelDataTo(depthPixels);
+
+            //get min and max reliable depth
+            int minDepth = frame.MinDepth;
+            int maxDepth = frame.MaxDepth;
+
+            //convert depth to RGB
+            int colorPixelIndex = 0;
+            for(int i = 0; i < depthPixels.Length; ++i)
+            {
+                //get depth for this pixel
+                short depth = depthPixels[i].Depth;
+
+                // To convert to a byte, we're discarding the most-significant
+                // rather than least-significant bits.
+                // We're preserving detail, although the intensity will "wrap."
+                // Values outside the reliable depth range are mapped to 0 (black).
+
+                // Note: Using conditionals in this loop could degrade performance.
+                // Consider using a lookup table instead when writing production code.
+                // See the KinectDepthViewer class used by the KinectExplorer sample
+                // for a lookup table example.
+
+                byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
+
+
+                //write out blue byte
+                colorPixels[colorPixelIndex++] = intensity;
+
+                //write out green byte
+                colorPixels[colorPixelIndex++] = intensity;
+
+                //write out red byte
+                colorPixels[colorPixelIndex++] = intensity;
+
+                // We're outputting BGR, the last byte in the 32 bits is unused so skip it
+                // If we were outputting BGRA, we would write alpha here.
+                ++colorPixelIndex;
+            }
+
+            
+
+            var stride = frame.Width * frame.BytesPerPixel;
+
+            var bmpFrame = new Bitmap(frame.Width, frame.Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+            var bmpData = bmpFrame.LockBits(new Rectangle(0, 0, frame.Width, frame.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, bmpFrame.PixelFormat);
+
+            System.Runtime.InteropServices.Marshal.Copy(colorPixels, 0, bmpData.Scan0, colorPixels.Length);
+
+            bmpFrame.UnlockBits(bmpData);
+
+            return bmpFrame;
+
+        }
+
+        private System.Drawing.Bitmap BitmapFromSource(BitmapSource bitmapsource)
+        {
+            System.Drawing.Bitmap bitmap;
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+
+                enc.Frames.Add(BitmapFrame.Create(bitmapsource));
+                enc.Save(outStream);
+                bitmap = new System.Drawing.Bitmap(outStream);
+            }
+            return bitmap;
+        }
+
     }
 }
