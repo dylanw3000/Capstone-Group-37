5af7ba9b9e3d10fcbaec59edd4f256c98fa60ddd
Benjamin Wick <benny.wick@gmail.com>
2017-04-10 18:00:04 -0700

    updated code and project

---
+++ videoRadar/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace videoRadar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btn_kinect_stream = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(4, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(453, 219);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.optionToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(923, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(115, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(115, 26);
+            this.loadToolStripMenuItem.Text = "load";
+            // 
+            // optionToolStripMenuItem
+            // 
+            this.optionToolStripMenuItem.Name = "optionToolStripMenuItem";
+            this.optionToolStripMenuItem.Size = new System.Drawing.Size(67, 24);
+            this.optionToolStripMenuItem.Text = "Option";
+            // 
+            // btn_kinect_stream
+            // 
+            this.btn_kinect_stream.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_kinect_stream.Location = new System.Drawing.Point(721, 516);
+            this.btn_kinect_stream.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_kinect_stream.Name = "btn_kinect_stream";
+            this.btn_kinect_stream.Size = new System.Drawing.Size(157, 28);
+            this.btn_kinect_stream.TabIndex = 4;
+            this.btn_kinect_stream.Text = "Stream";
+            this.btn_kinect_stream.UseVisualStyleBackColor = true;
+            this.btn_kinect_stream.Click += new System.EventHandler(this.kinect_stream_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Status:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(96, 34);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(13, 17);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "-";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(465, 4);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(454, 219);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.imageBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.imageBox2, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 54);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(923, 455);
+            this.tableLayoutPanel1.TabIndex = 8;
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox1.Location = new System.Drawing.Point(3, 230);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(455, 222);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox2.Location = new System.Drawing.Point(464, 230);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(456, 222);
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(923, 559);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_kinect_stream);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionToolStripMenuItem;
+        private System.Windows.Forms.Button btn_kinect_stream;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+    }
+}
+
---
+++ videoRadar/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+// Kinect includes
+using Microsoft.Kinect;
+using Coding4Fun.Kinect.WinForm;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.IO;
+
+// EmguCV includes
+using Emgu.CV;
+using Emgu.CV.Cvb;
+using Emgu.CV.UI;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace videoRadar
+{
+    public partial class Form1 : Form
+    {
+        // Kinect member variables
+        private KinectSensor kSensor;
+
+        // EmguCV member variables
+        Mat originalImage;
+        Mat processedImage;
+        Mat hsvImage;
+        Mat upper_red_hue_range;
+        Mat lower_red_hue_range;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void ProcessFrame()
+        {
+            // Create camera instance
+            // originalImage = captureCamera.QueryFrame();
+
+            // Create instances
+            hsvImage = new Mat(originalImage.Size, DepthType.Cv8U, 3);
+            upper_red_hue_range = new Mat(originalImage.Size, DepthType.Cv8U, 1);
+            lower_red_hue_range = new Mat(originalImage.Size, DepthType.Cv8U, 1);
+            processedImage = new Mat(originalImage.Size, DepthType.Cv8U, 1);
+
+            // Convert to HSV image
+            CvInvoke.CvtColor(originalImage, hsvImage, ColorConversion.Bgr2Hsv);
+
+            // Create range of hue value for red and then add both to the processedImage
+            CvInvoke.InRange(hsvImage, new ScalarArray(new MCvScalar(0, 155, 155)), new ScalarArray(new MCvScalar(18, 255, 255)), lower_red_hue_range);
+            CvInvoke.InRange(hsvImage, new ScalarArray(new MCvScalar(160, 155, 155)), new ScalarArray(new MCvScalar(179, 255, 255)), upper_red_hue_range);
+
+            CvInvoke.Add(lower_red_hue_range, upper_red_hue_range, processedImage);
+
+            //
+            CvInvoke.GaussianBlur(processedImage, processedImage, new Size(3, 3), 0);
+
+            Mat structuringElement = CvInvoke.GetStructuringElement(ElementShape.Rectangle, new Size(3, 3), new Point(-1, -1));
+
+            CvInvoke.Dilate(processedImage, processedImage, structuringElement, new Point(-1, -1), 1, BorderType.Default, new MCvScalar(0, 0, 0));
+            CvInvoke.Erode(processedImage, processedImage, structuringElement, new Point(-1, -1), 1, BorderType.Default, new MCvScalar(0, 0, 0));
+
+            CircleF[] circles = CvInvoke.HoughCircles(processedImage, HoughType.Gradient, 2.0, processedImage.Rows / 4, 100, 50, 10, 400);
+
+            // ***IMPORTANT*** right now it is causing a lot of lag. Currently trying to fix. ********
+            //foreach (CircleF circle in circles)
+            //{
+            //    //if (textBox1.Text != "")
+            //    //{                         // if we are not on the first line in the text box
+            //    //    textBox1.AppendText(Environment.NewLine);         // then insert a new line char
+            //    //}
+
+            //    //textBox1.AppendText("ball position x = " + circle.Center.X.ToString().PadLeft(4) + ", y = " + circle.Center.Y.ToString().PadLeft(4) + ", radius = " + circle.Radius.ToString("###.000").PadLeft(7));
+            //    //textBox1.ScrollToCaret();             // scroll down in text box so most recent line added (at the bottom) will be shown
+
+            //    CvInvoke.Circle(originalImage, new Point((int)circle.Center.X, (int)circle.Center.Y), (int)circle.Radius, new MCvScalar(0, 0, 255), 2);
+            //    CvInvoke.Circle(originalImage, new Point((int)circle.Center.X, (int)circle.Center.Y), 3, new MCvScalar(0, 255, 0), -1);
+            //}
+
+            imageBox1.Image = originalImage;
+            imageBox2.Image = upper_red_hue_range;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void FinalVideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            //throw new NotImplementedException();
+            Bitmap image = (Bitmap)eventArgs.Frame.Clone(); //bitmap boxing
+            pictureBox1.Image = image;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //stop webcam when form closes
+            if (kSensor != null && kSensor.IsRunning)
+            {
+                kSensor.Stop();
+            }
+
+        }
+
+        private void kinect_stream_Click(object sender, EventArgs e)
+        {
+            if (btn_kinect_stream.Text == "Stream")
+            {
+                if (KinectSensor.KinectSensors.Count > 0)
+                {
+                    this.btn_kinect_stream.Text = "Stop";
+                    kSensor = KinectSensor.KinectSensors[0];
+                    KinectSensor.KinectSensors.StatusChanged += KinectSensors_StatusChanged;
+                }
+
+                kSensor.Start();
+                kSensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                kSensor.ColorFrameReady += KSensor_ColorFrameReady;
+
+                kSensor.DepthStream.Enable();
+                kSensor.DepthStream.Range = DepthRange.Default;
+                kSensor.DepthFrameReady += KSensor_DepthFrameReady;
+
+            }
+            else
+            {
+                if(kSensor != null && kSensor.IsRunning)
+                {
+                    kSensor.Stop();
+                    this.btn_kinect_stream.Text = "Stream";
+                    this.pictureBox1.Image = null;
+                }
+            }
+
+            
+        }
+
+        private void KSensor_DepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
+        {
+            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+            {
+                if(depthFrame != null)
+                {
+                    //pictureBox1.Image = CreateColorBitmapFromDepth(depthFrame);
+
+                    byte[] pixels = GenerateColoredBytes(depthFrame);
+
+                    //number of bytes per row width * 4 (B,G,R, Empty)
+                    int stride = depthFrame.Width * 4;
+
+                    //create image
+                    pictureBox1.Image = BitmapFromSource( BitmapSource.Create(depthFrame.Width, depthFrame.Height, 96, 96, PixelFormats.Bgr32, null, pixels, stride) );
+                }
+            }
+        }
+
+        private void KSensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            using (var frame = e.OpenColorImageFrame())
+            {
+                if (frame != null)
+                {
+                    //pictureBox2.Image = CreateBitmapFromSensor(frame);
+                }
+            }
+
+            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+            {
+                if(depthFrame != null)
+                {
+                    //byte[] pixels = GenerateColoredBytes(depthFrame);
+
+                    //number of bytes per row width * 4 (B,G,R, Empty)
+                    int stride = depthFrame.Width * 4;
+                }
+            }
+        }
+
+        private byte[] GenerateColoredBytes(DepthImageFrame depthFrame)
+        {
+
+            //get min and max reliable depth
+            int minDepth = depthFrame.MinDepth;
+            int maxDepth = depthFrame.MaxDepth;
+
+            //get the raw data from kinect with the depth for evey pixel
+            short[] rawDepthData = new short[depthFrame.PixelDataLength];
+            depthFrame.CopyPixelDataTo(rawDepthData);
+
+            //using the depthFrame to creat the image to display on-screen
+            //depthFrame contains color information for all pixel in image
+            //Height x Width x 4 (Red, Green, Blue, empty byte)
+            Byte[] pixels = new byte[depthFrame.Height * depthFrame.Width * 4];
+
+            //Bgr32  - Blue, Green, Red. empty byte
+            //Bgra32 - Blue, Green, Red, transparency
+            //you must set transparency for Bgra as .NET defaults a byte to 0 = fully transparent
+
+            //hardcoded location to Blue, Green, Red (BGR) index postitions
+            const int BlueIndex = 0;
+            const int GreenIndex = 1;
+            const int RedIndex = 2;
+
+            //loop through all distances
+            //pick a RGB color based on distance
+            for (int depthIndex = 0, colorIndex = 0;
+                depthIndex < rawDepthData.Length && colorIndex < pixels.Length;
+                depthIndex++, colorIndex += 4)
+            {
+                //get the depth value
+                int depth = rawDepthData[depthIndex] >> DepthImageFrame.PlayerIndexBitmaskWidth;
+
+                //equal coloring for the monochromatic histogram
+                byte intensity = CalculateIntesityFromDepth(depth, minDepth, maxDepth);
+                pixels[colorIndex + BlueIndex] = intensity;
+                pixels[colorIndex + GreenIndex] = intensity;
+                pixels[colorIndex + RedIndex] = intensity;
+            }
+
+            return pixels;
+        }
+
+        private byte CalculateIntesityFromDepth(int distance, int MinDepthDistance, int MaxDepthDistance)
+        {
+            //formula for calculating monochrome intensity for histogram
+            return (byte)(255 - (255 * Math.Max(distance - MinDepthDistance, 0)
+                / (MaxDepthDistance)));
+        }
+
+        private void KSensor_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (var frame = e.OpenColorImageFrame())
+            {
+                if (frame != null)
+                {
+                    //create bitmap from sensor
+                    Bitmap bitmap = CreateBitmapFromSensor(frame);
+                    Image<Bgr, Byte> imageCV = new Image<Bgr, byte>(bitmap); //Image Class from Emgu.CV
+                    originalImage = imageCV.Mat; //This is your Image converted to Mat
+                    ProcessFrame();
+                }
+            }
+        }
+
+        private void KinectSensors_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            this.lblStatus.Text = kSensor.Status.ToString();
+        }
+
+        private Bitmap CreateBitmapFromSensor(ColorImageFrame frame)
+        {
+            //WriteableBitmap map;
+            var pixelData = new byte[frame.PixelDataLength];
+            frame.CopyPixelDataTo(pixelData);
+
+            // return pixelData.ToBitmap(frame.Width, frame.Height);
+            var stride = frame.Width * frame.BytesPerPixel;
+
+            var bmpFrame = new Bitmap(frame.Width, frame.Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+            var bmpData = bmpFrame.LockBits(new Rectangle(0, 0, frame.Width, frame.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, bmpFrame.PixelFormat);
+
+            System.Runtime.InteropServices.Marshal.Copy(pixelData, 0, bmpData.Scan0, frame.PixelDataLength);
+
+            bmpFrame.UnlockBits(bmpData);
+
+            return bmpFrame;
+        }
+
+        private Bitmap CreateColorBitmapFromDepth(DepthImageFrame frame)
+        {
+            //WriteableBitmap colorBitmap;
+            DepthImagePixel[] depthPixels;
+            byte[] colorPixels;
+
+            depthPixels = new DepthImagePixel[kSensor.DepthStream.FramePixelDataLength];
+            colorPixels = new byte[kSensor.DepthStream.FramePixelDataLength * sizeof(int)];
+
+            frame.CopyDepthImagePixelDataTo(depthPixels);
+
+            //get min and max reliable depth
+            int minDepth = frame.MinDepth;
+            int maxDepth = frame.MaxDepth;
+
+            //convert depth to RGB
+            int colorPixelIndex = 0;
+            for(int i = 0; i < depthPixels.Length; ++i)
+            {
+                //get depth for this pixel
+                short depth = depthPixels[i].Depth;
+
+                // To convert to a byte, we're discarding the most-significant
+                // rather than least-significant bits.
+                // We're preserving detail, although the intensity will "wrap."
+                // Values outside the reliable depth range are mapped to 0 (black).
+
+                // Note: Using conditionals in this loop could degrade performance.
+                // Consider using a lookup table instead when writing production code.
+                // See the KinectDepthViewer class used by the KinectExplorer sample
+                // for a lookup table example.
+
+                byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
+
+                //write out blue byte
+                colorPixels[colorPixelIndex++] = intensity;
+
+                //write out green byte
+                colorPixels[colorPixelIndex++] = intensity;
+
+                //write out red byte
+                colorPixels[colorPixelIndex++] = intensity;
+
+                // We're outputting BGR, the last byte in the 32 bits is unused so skip it
+                // If we were outputting BGRA, we would write alpha here.
+                ++colorPixelIndex;
+            }
+
+            var stride = frame.Width * frame.BytesPerPixel;
+
+            var bmpFrame = new Bitmap(frame.Width, frame.Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+            var bmpData = bmpFrame.LockBits(new Rectangle(0, 0, frame.Width, frame.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, bmpFrame.PixelFormat);
+
+            System.Runtime.InteropServices.Marshal.Copy(colorPixels, 0, bmpData.Scan0, colorPixels.Length);
+
+            bmpFrame.UnlockBits(bmpData);
+
+            return bmpFrame;
+
+        }
+
+        private System.Drawing.Bitmap BitmapFromSource(BitmapSource bitmapsource)
+        {
+            System.Drawing.Bitmap bitmap;
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+
+                enc.Frames.Add(BitmapFrame.Create(bitmapsource));
+                enc.Save(outStream);
+                bitmap = new System.Drawing.Bitmap(outStream);
+            }
+
+            return bitmap;
+        }
+    }
+}
